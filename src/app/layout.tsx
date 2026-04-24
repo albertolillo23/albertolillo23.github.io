71416f104bcc869d0837ea388dc60e4d49ab07bd
alberto <alby5062@gmail.com>
2026-04-24 12:45:05 +0200

    feat: replace favicon.ico with SVG icon in layout metadata and root directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,9 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🤖</text></svg>',
+  },
 };
 
 export default function RootLayout({
